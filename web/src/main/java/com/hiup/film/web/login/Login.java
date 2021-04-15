76e23e6515b42dfd00c8e60a0dc7172d79906e08
Jicgiebcden <hevqiang86@163.com>
2021-04-15 15:25:20 +0800

    提交

---
+++ web/src/main/java/com/hiup/film/web/login/Login.java
@@ -0,0 +1,48 @@
+package com.hiup.film.web.login;
+/**
+ * Created by zw on 2017/12/25.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * code is far away from bug with the animal protecting
+ * ┏┓　　　┏┓
+ * ┏┛┻━━━┛┻┓
+ * ┃　　　　　　　┃
+ * ┃　　　━　　　┃
+ * ┃　┳┛　┗┳　┃
+ * ┃　　　　　　　┃
+ * ┃　　　┻　　　┃
+ * ┃　　　　　　　┃
+ * ┗━┓　　　┏━┛
+ * 　　┃　　　┃神兽保佑
+ * 　　┃　　　┃代码无BUG！
+ * 　　┃　　　┗━━━┓
+ * 　　┃　　　　　　　┣┓
+ * 　　┃　　　　　　　┏┛
+ * 　　┗┓┓┏━┳┓┏┛
+ * 　　　┃┫┫　┃┫┫
+ * 　　　┗┻┛　┗┻┛
+ *
+ * <p>
+ * ---------------------------------
+ */
+@SpringBootApplication
+@Configuration
+@RestController
+public class Login {
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Login.class,args);
+    }
+
+}
